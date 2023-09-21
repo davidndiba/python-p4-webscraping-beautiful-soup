45d6dfa8f392907d4c38031352d862282f45b897
David <david.ndiba@student.moringaschool.com>
2023-09-21 12:38:25 +0300

    webscraping with beautiful soup

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,11 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+courses = doc.select('.heading-60-black.color-black.mb-20')
+doc.select('.heading-60-black.color-black.mb-20')[0].attrs
+
+for course in courses: 
+  print(course.contents[0].strip())
